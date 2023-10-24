b7e3eee2e2a7e8e0712e7c7aec58defb4af72611
akleem <74227145+akleem-AA@users.noreply.github.com>
2023-10-25 03:42:51 +0530

    Update reverseOnlyChar.js
    added one more solution

--- reverseOnlyChar.js
+++ reverseOnlyChar.js
@@ -1,9 +1,31 @@
-let str = "only change character";
+let input = "only change character";
 //output  ylno egnahc retcarahc
 
-function convert(str){
+//answer algorithm
+// We first split the input sentence into words.
+// We use a for loop to iterate through each word in the array of words.
+// For each word, we use another for loop to reverse the characters within that word.
+// The reversed word is then pushed into the final array.
+// Finally, we join the final arry words back into a sentence.
+
+//step:1 by using map
+function reverseOnlyChar1(data){
+    let words = data.split(' ')
+    
+    const char  = words.map((item)=>{
+      return  item.split('').reverse().join('')
+    })
+    return char.join(' ')
+}
+
+let mapResult = reverseOnlyChar1(input)
+console.log(mapResult) //output is same ylno egnahc rahc
+
+
+//step 2 by using for loop
+function reverseChar(str){
     let words = str.split(' ')
-    let final = []
+    let final = [];
     for(let i = 0 ; i<words.length;i++){
         let word = words[i]
         let reversed = ''
@@ -14,11 +36,6 @@ function convert(str){
     }
     return final.join(' ')
 }
-let result  = convert(str)
-console.log("final change only char =",result)
+let result  = reverseChar(input)
+console.log("final change only char =",result) //oupt ylno egnahc rahc
 
-// We first split the input sentence into words.
-// We use a for loop to iterate through each word in the array of words.
-// For each word, we use another for loop to reverse the characters within that word.
-// The reversed word is then pushed into the final array.
-// Finally, we join the final arry words back into a sentence.
